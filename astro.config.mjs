fa81de90fd601f66e3fe09873c1a7481a08221b1
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-08-29 13:33:34 +0200

    back to original template code https://github.com/mhyfritz/astro-landing-page.git

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,18 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), preact()],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
   vite: {
     ssr: {
-      external: ["svgo"]
-    }
-  }
+      external: ["svgo"],
+    },
+  },
 });

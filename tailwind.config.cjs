e6afdba01c78a45fd687ab6f9ab5458833803c75
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-09-05 03:55:36 +0200

    added again the minimal animations, removed usage of big dependency tree between preact components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -36,5 +36,5 @@ module.exports = {
   corePlugins: {
     fontSize: false,
   },
-  plugins: [require("tailwindcss-fluid-type")], //, require('tailwindcss-animated')
+  plugins: [require("tailwindcss-fluid-type"), require('tailwindcss-animated')], //, require('tailwindcss-animated')
 };

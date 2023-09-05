c32ba46b7ae5009555bf66acf5aeb21c87371bcd
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-09-05 05:06:00 +0200

    added ananimated typing to onair widget

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,28 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }  
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }  
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
+      },
       /* animationDelay: { // for 'tailwindcss-animated' plugin config.
         275: '275ms',
         5000: '5s',
